4253a777bc9572be00ce8db781a8e781543dd8ae
prisis <d.bannert@anolilab.de>
2021-11-11 23:32:18 +0100

    chore: fixed node version

--- verify-node-version.cjs
+++ verify-node-version.cjs
@@ -10,7 +10,7 @@ if (process.env.SKIP_CHECK !== undefined) {
     process.exit(0);
 }
 
-if (process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
+if (requiredVersion.split(".")[0] >= process.version.split(".")[0]) {
     console.error(`[!] This project requires Node.js ${requiredVersion}, current version is ${process.version}`);
 
     process.exit(1);
